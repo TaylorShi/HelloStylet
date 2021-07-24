bb5ffdec2a5ecc97f3fc5930d659225528dc7a80
YongAn <414675056@qq.com>
2021-07-24 22:02:58 +0800

    添加双向事件绑定

--- HelloStyletClient/Pages/ShellViewModel.cs
+++ HelloStyletClient/Pages/ShellViewModel.cs
@@ -4,5 +4,24 @@ namespace HelloStyletClient.Pages
 {
     public class ShellViewModel : Screen
     {
+        /// <summary>
+        /// 欢迎词
+        /// </summary>
+        /// <value></value>
+        public string WelcomeWord { get; set; } = "Hello Stylet!";
+
+        /// <summary>
+        /// 能否提交
+        /// </summary>
+        /// <value></value>
+        public bool IsCanSubmitMe => !string.IsNullOrEmpty(WelcomeWord);
+
+        /// <summary>
+        /// 提交事件
+        /// </summary>
+        public void SubmitMe()
+        {
+            WelcomeWord += " 提交";
+        }
     }
 }
